b139a0bf2b73bc845cd197e2f205d74dc0c809c5
phanvanbinh07516 <binhpvpd07516@fpt.edu.vn>
2023-04-25 07:12:37 +0700

    nop asgm

---
+++ asgm/asgm.js
@@ -0,0 +1,26 @@
+
+function bigImg(x) {
+x.style.height = "330px";
+x.style.width = "305px";
+}
+
+function normalImg(x) {
+x.style.height = "300px";
+x.style.width = "300px";
+}
+function text(x) {
+ x.style.color = "blue";
+ 
+}
+    
+function normaltext(x) {
+x.style.color = "white";
+    }
+function item(x) {
+x.style.style = "red";
+ }
+        
+function normalitem(x) {
+x.style.style = "darkgrey";
+
+}
